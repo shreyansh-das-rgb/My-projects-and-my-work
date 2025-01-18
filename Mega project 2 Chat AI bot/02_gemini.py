526e7f24ab724892c1c4f4beb65075a2fb091ee3
Shreyansh Das <143320251+shreyansh-das-rgb@users.noreply.github.com>
2025-01-18 13:56:49 +0530

    Update 02_gemini.py

--- Mega project 2 Chat AI bot/02_gemini.py
+++ Mega project 2 Chat AI bot/02_gemini.py
@@ -8,7 +8,7 @@ command = '''
 [8:42 am, 16/1/2025] Shreyansh Das: https://youtu.be/UrsmFxEIp5k?si=BiYyhn3N7p2U61UQ
 [8:42 am, 16/1/2025] Shreyansh Das: This video is helpful
 '''
-genai.configure(api_key="AIzaSyCKG6YFc23MhplPbSdcVuxrCrxPVjhT1zQ")
+genai.configure(api_key="GIVE UR API KEY")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content(f"(Your role is Shreyansh Another profile here and you speak hindi as well as english and is a programmer.You will analyse the chat the give answer as shreyansh another profile so here is the chat) {command} (and i also forgot to tell pretend like u are shreyansh another profile and ONLY answer the chat as Shreyansh another profile no more extra comments)(and also speak only in one language)")
 print(response.text)
